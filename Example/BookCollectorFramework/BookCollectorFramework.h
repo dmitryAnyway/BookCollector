c4265c08ccef910439eefa77b53775a7c5d86daf
ext.dzubarev <ext.dzubarev@tinkoff.ru>
2023-07-30 19:23:56 +0300

    add frameworkinside

---
+++ Example/BookCollectorFramework/BookCollectorFramework.h
@@ -0,0 +1,19 @@
+//
+//  BookCollectorFramework.h
+//  BookCollectorFramework
+//
+//  Created by Дмитрий on 30.07.2023.
+//  Copyright © 2023 ext.dzubarev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BookCollectorFramework.
+FOUNDATION_EXPORT double BookCollectorFrameworkVersionNumber;
+
+//! Project version string for BookCollectorFramework.
+FOUNDATION_EXPORT const unsigned char BookCollectorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookCollectorFramework/PublicHeader.h>
+
+#import <BookCollectorFramework/BookCollector.h>
